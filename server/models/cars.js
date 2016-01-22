77ba3df9c6944bc9684bfbd9ecaabf2d54787488
kevin <kevintxhe933@hotmail.com>
2016-01-22 13:53:08 -0800

    added footer and logout

---
+++ server/models/cars.js
@@ -0,0 +1,482 @@
+module.exports = [
+        {
+          "name": "Acura",
+          "country": "Japan",
+          "logo": "images/brands/acura.png",
+          "models":[
+            {
+              "name": "NSX",
+              "pic": "images/models/acura/nsx.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Alfa_Romeo",
+          "country": "Italy",
+          "logo": "images/brands/alfa_romeo.jpg",
+          "models":[
+            {
+              "name": "Alfa Romeo 4c",
+              "pic": "images/models/alfa_romeo/alfa_romeo_4c.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Aston_Martin",
+          "country": "United Kingdom",
+          "logo": "images/brands/aston_martin.png",
+          "models":[
+            {
+              "name": "Vantage",
+              "pic": "images/models/aston_martin/vantage.jpg"
+            },
+            {
+              "name": "DB9",
+              "pic": "images/models/aston_martin/Db9.jpg"
+            },
+            {
+              "name": "Vulcan",
+              "pic": "images/models/aston_martin/vulcan.jpg"
+            },
+            {
+              "name": "One-77",
+              "pic": "images/models/aston_martin/one-77.jpg"
+            },
+            {
+              "name": "Vanquish",
+              "pic": "images/models/aston_martin/vanquish.jpg"
+            },
+            {
+              "name": "Rapide",
+              "pic": "images/models/aston_martin/rapide.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Audi",
+          "country": "German",
+          "logo": "images/brands/audi.png",
+          "models":[
+            {
+              "name": "S5",
+              "pic": "images/models/audi/s5.jpg"
+            },
+            {
+              "name": "S6",
+              "pic": "images/models/audi/s6.jpg"
+            },
+            {
+              "name": "RS7",
+              "pic": "images/models/audi/rs7.jpg"
+            },
+            {
+              "name": "TTS",
+              "pic": "images/models/audi/tts.jpg"
+            },
+            {
+              "name": "R8",
+              "pic": "images/models/audi/r8.jpg"
+            },
+            {
+              "name": "S8",
+              "pic": "images/models/audi/s8.jpg"
+            },
+            {
+              "name": "SQ5",
+              "pic": "images/models/audi/sq5.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Bentley",
+          "country": "United Kingdom",
+          "logo": "images/brands/bentley.png",
+          "models":[
+            {
+              "name": "Continental",
+              "pic": "images/models/bentley/continental.jpg"
+            },
+            {
+              "name": "Mulsanne",
+              "pic": "images/models/bentley/mulsanne.jpg"
+            },
+            {
+              "name": "Bentayga",
+              "pic": "images/models/bentley/bentayga.jpg"
+            },
+            {
+              "name": "Flying Spur",
+              "pic": "images/models/bentley/flying_spur.jpg"
+            }
+          ]
+        },
+        {
+          "name": "BMW",
+          "country": "German",
+          "logo": "images/brands/bmw.png",
+          "models":[
+            {
+              "name": "M2",
+              "pic": "images/models/bmw/m2.jpg"
+            },
+            {
+              "name": "M3",
+              "pic": "images/models/bmw/m3.jpg"
+            },
+            {
+              "name": "M4",
+              "pic": "images/models/bmw/m4.jpg"
+            },
+            {
+              "name": "M5",
+              "pic": "images/models/bmw/m5.jpg"
+            },
+            {
+              "name": "M6",
+              "pic": "images/models/bmw/m6.jpg"
+            },
+            {
+              "name": "X5M",
+              "pic": "images/models/bmw/x5m.jpg"
+            },
+            {
+              "name": "X6M",
+              "pic": "images/models/bmw/x6m.jpg"
+            },
+            {
+              "name": "i8",
+              "pic": "images/models/bmw/i8.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Bugatti",
+          "country": "France",
+          "logo": "images/brands/bugatti.png",
+          "models":[
+            {
+              "name": "Veyron",
+              "pic": "images/models/bugatti/veyron.jpg"
+            },
+          ]
+        },
+        {
+          "name": "Cadillac",
+          "country": "United States",
+          "logo": "images/brands/cadillac.jpg",
+          "models":[
+            {
+              "name": "ATS V",
+              "pic": "images/models/cadillac/ats-v.jpg"
+            },
+            {
+              "name": "Elmiraj",
+              "pic": "images/models/cadillac/elmiraj.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Chevrolet",
+          "country": "United States",
+          "logo": "images/brands/chevrolet.jpg",
+          "models":[
+            {
+              "name": "Corvette",
+              "pic": "images/models/chevrolet/corvette.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Ferrari",
+          "country": "Italy",
+          "logo": "images/brands/ferrari.jpg",
+          "models":[
+            {
+              "name": "458",
+              "pic": "images/models/ferrari/458.jpg"
+            },
+            {
+              "name": "488",
+              "pic": "images/models/ferrari/488.jpg"
+            },
+            {
+              "name": "F430",
+              "pic": "images/models/ferrari/f430.jpg"
+            },
+            {
+              "name": "California",
+              "pic": "images/models/ferrari/california.jpg"
+            },
+            {
+              "name": "FF",
+              "pic": "images/models/ferrari/ff.jpg"
+            },
+            {
+              "name": "599",
+              "pic": "images/models/ferrari/599.jpg"
+            },
+            {
+              "name": "F12 Berlinetta",
+              "pic": "images/models/ferrari/f12-berlinetta.jpg"
+            },
+            {
+              "name": "LaFerrari",
+              "pic": "images/models/ferrari/laferrari.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Ford",
+          "country": "United States",
+          "logo": "images/brands/ford.jpg",
+          "models":[
+            {
+              "name": "GT",
+              "pic": "images/models/ford/gt.jpg"
+            },
+            {
+              "name": "Mustang",
+              "pic": "images/models/ford/mustang.jpg"
+            },
+            {
+              "name": "Mustang Shelby",
+              "pic": "images/models/ford/mustang-shelby.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Jaguar",
+          "country": "United Kingdom",
+          "logo": "images/brands/jaguar.jpg",
+          "models":[
+            {
+              "name": "F-TYPE",
+              "pic": "images/models/jaguar/f-type.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Jeep",
+          "country": "United States",
+          "logo": "images/brands/jeep.png",
+          "models":[
+            {
+              "name": "Grand Cherokee SRT",
+              "pic": "images/models/jeep/grand-cherokee-srt.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Koenigsegg",
+          "country": "Sweden",
+          "logo": "images/brands/koenigsegg.jpg",
+          "models":[
+            {
+              "name": "Agera R",
+              "pic": "images/models/koenigsegg/agera-r.jpg"
+            },
+            {
+              "name": "One 1",
+              "pic": "images/models/koenigsegg/one-1.jpg"
+            },
+            {
+              "name": "Regera",
+              "pic": "images/models/koenigsegg/regera.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Lamborghini",
+          "country": "Italy",
+          "logo": "images/brands/lamborghini.png",
+          "models":[
+            {
+              "name": "Aventador",
+              "pic": "images/models/lamborghini/aventador.jpg"
+            },
+            {
+              "name": "Huracan",
+              "pic": "images/models/lamborghini/huracan.jpg"
+            },
+            {
+              "name": "Reventon",
+              "pic": "images/models/lamborghini/reventon.jpg"
+            },
+            {
+              "name": "Gallardo",
+              "pic": "images/models/lamborghini/gallardo.jpg"
+            },
+            {
+              "name": "Veneno",
+              "pic": "images/models/lamborghini/veneno.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Land Rover",
+          "country": "United Kingdom",
+          "logo": "images/brands/land_rover.png",
+          "models":[
+            {
+              "name": "Range Rover",
+              "pic": "images/models/land_rover/range_rover.jpg"
+            },
+            {
+              "name": "Evoque",
+              "pic": "images/models/land_rover/evoque.jpg"
+            },
+            {
+              "name": "LR4",
+              "pic": "images/models/land_rover/lr4.jpg"
+            },
+            {
+              "name": "Discovery",
+              "pic": "images/models/land_rover/discovery.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Lexus",
+          "country": "Japan",
+          "logo": "images/brands/lexus.png",
+          "models":[
+            {
+              "name": "LFA",
+              "pic": "images/models/lexus/lfa.jpg"
+            },
+            {
+              "name": "GS F",
+              "pic": "images/models/lexus/gs_f.jpg"
+            },
+            {
+              "name": "RC F",
+              "pic": "images/models/lexus/rc_f.jpg"
+            },
+            {
+              "name": "IS F",
+              "pic": "images/models/lexus/is_f.jpg"
+            },
+            {
+              "name": "LF LC",
+              "pic": "images/models/lexus/lf_lc.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Lotus",
+          "country": "United Kingdom",
+          "logo": "images/brands/lotus.jpg",
+          "models":[
+            {
+              "name": "Elise",
+              "pic": "images/models/lotus/elise.jpg"
+            },
+            {
+              "name": "Exige",
+              "pic": "images/models/lotus/exige.jpg"
+            },
+            {
+              "name": "Evora",
+              "pic": "images/models/lotus/evora.jpg"
+            },
+            {
+              "name": "3Eleven",
+              "pic": "images/models/lotus/3eleven.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Maserati",
+          "country": "Italy",
+          "logo": "images/brands/maserati.png",
+          "models":[
+            {
+              "name": "Ghibli",
+              "pic": "images/models/maserati/ghibli.jpg"
+            },
+            {
+              "name": "GranTurismo",
+              "pic": "images/models/maserati/granturismo.jpg"
+            },
+            {
+              "name": "GranCabrio",
+              "pic": "images/models/maserati/grancabrio.jpg"
+            },
+            {
+              "name": "Quattroporte",
+              "pic": "images/models/maserati/quattroporte.jpg"
+            },
+            {
+              "name": "MC12",
+              "pic": "images/models/maserati/mc12.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Maybach",
+          "country": "German",
+          "logo": "images/brands/maybach.png",
+          "models":[
+            {
+              "name": "Mercedes-Maybach S600",
+              "pic": "images/models/maybach/mercedes_maybach_s600.jpg"
+            }
+          ]
+        },
+        {
+          "name": "McLaren",
+          "country": "United Kingdom",
+          "logo": "images/brands/mclaren.jpg",
+          "models":[
+            {
+              "name": "McLaren P1",
+              "pic": "images/models/mclaren/mclaren_p1.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Mercedes-Benz",
+          "country": "German",
+          "logo": "images/brands/mercedes_benz.jpg"
+        },
+        {
+          "name": "Mitsubishi",
+          "country": "Japan",
+          "logo": "images/brands/mitsubishi.jpg"
+        },
+        {
+          "name": "Nissan",
+          "country": "Japan",
+          "logo": "images/brands/nissan.jpg"
+        },
+        {
+          "name": "Pagani",
+          "country": "Italy",
+          "logo": "images/brands/pagani.jpg"
+        },
+        {
+          "name": "Porsche",
+          "country": "German",
+          "logo": "images/brands/porsche.jpg"
+        },
+        {
+          "name": "Rolls-Royce",
+          "country": "United Kingdom",
+          "logo": "images/brands/rolls_royce.jpg"
+        },
+        {
+          "name": "Subaru",
+          "country": "Japan",
+          "logo": "images/brands/subaru.jpg"
+        },
+        {
+          "name": "Tesla",
+          "country": "United States",
+          "logo": "images/brands/tesla.jpg"
+        },
+        {
+          "name": "Toyota",
+          "country": "Japan",
+          "logo": "images/brands/toyota.jpg"
+        }
+    ]
--- server/server.routes.js
+++ server/server.routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var request = require('request');
 var passport = require('passport');
+var cars = require('./models/cars')
 
 module.exports = function() {
   var router = express.Router();
@@ -10,11 +11,13 @@ module.exports = function() {
     if(!req.user){
       res.render('index', {
         page: 'Home',
+        cars: cars,
         user: req.user
       });
     }else{
       res.render('index', {
         page: 'Home',
+        cars: cars,
         user: req.user
       });
     };
@@ -23,33 +26,14 @@ module.exports = function() {
   router.get('/oauth/instagram', passport.authenticate('instagram'));
 
   router.get('/callback', 
-  passport.authenticate('instagram', { failureRedirect: '/login' }),
+  passport.authenticate('instagram', { failureRedirect: '/' }),
   function(req, res) {
-    // Successful authentication, redirect home.
-    console.log(JSON.stringify(req.user)+"~~~~~~~~~~~~~~~~~~user");
-    //res.json(JSON.stringify(req.user));
     res.redirect('/');
   });
 
-  /*router.get('/callback',function(req, res){
-    console.log(req.session);
-
-    var code = req.query.code;
-    var callbackReq = {
-      method: 'post',
-      body:'client_id=90ba6e4257404b0e87e28d9297d6ad93&client_secret=236275c7dce24640b986e4f7ed8f0fe5&grant_type=authorization_code&redirect_uri=http://www.supercarsta.com/callback&code=' + code,
-      url:'https://api.instagram.com/oauth/access_token'
-    };
-
-    request(callbackReq, function (err, result) {
-        if (err) {
-          console.log('Error :' ,err);
-          return
-        }
-        res.type('json');
-        res.send(result.body);
-        return;
-    });
-  });*/
+  router.get('/oauth/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
   return router;
 };
